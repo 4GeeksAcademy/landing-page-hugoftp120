d187ecaa1bd95998eceea98fcf198a00c6994c56
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-16 13:21:23 +0000

    surf

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,16 +1,19 @@
 
-const Card = () => {
-    const imgUrl = "https://media.istockphoto.com/id/2065673896/es/foto/rolling-dice-macro.jpg?s=612x612&w=0&k=20&c=gw61Xz6R9WUx65dUqmSErimq5ezPeHeEB-3AkYqpCEk=";
-
+const Card = (props) => {
+    console.log(props)
+    
+    
     return (
-        <div>
-            <div className="card" style={{ width: "20rem" }}>
-                <img src={imgUrl} className="card-img-top" alt="..." />
+        <>
+        <div className="col-md-3 mb-4">
+            <div className="Card" style={{ width: "20rem" }}>
+                <img src={props.imgUrl1} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
             </div>
         </div>
+        </>
     )
 }
 export default Card
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,29 +3,25 @@ import NavBar from "./NavBar";
 import Card from "./Card";
 import Jumbotron from "./Jumbotron";
 
+function Home() {
+const imgUrl1 = "https://media.istockphoto.com/id/2065673896/es/foto/rolling-dice-macro.jpg?s=612x612&w=0&k=20&c=gw61Xz6R9WUx65dUqmSErimq5ezPeHeEB-3AkYqpCEk=";
+const imgUrl2 = "https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2670"
+const imgUrl3 = "https://images.unsplash.com/photo-1616449973117-0e1d99c56ed3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074"
+const imgUrl4 = "https://images.unsplash.com/photo-1616141893496-fbc65370493e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070"
 const Home = () => {
 	return (
-		<div>
-			<NavBar />
-			<Jumbotron />
-			<div className="container my-4">
-				<div clasName="row justify-content-center">
-					<div className="col-md-3 mb-4">
-						<Card />
-					</div>
-					<div className="col-md3 mb-4">
-						<Card />
-					</div>
-					<div clasName="col-md-3 mb-4">
-						<Card />
-					</div>
-					<div className="col-md-3 mb-4">
-						<Card />
-					</div>
-				</div>
-			</div>
+	<>
+		<NavBar />
+		<div className="container ">
+		<Jumbotron />
+		<div clasName="justify-content-center d-flex mt-5">
+		<Card imageUrl={imgUrl1}/>
+		<Card imageUrl={imgUrl2}/>
+		<Card imageUrl={imgUrl3}/>
+		<Card imageUrl={imgUrl4}/>
 		</div>
-	);
+		</div>
+	</>);
 };
-
+}
 export default Home;
