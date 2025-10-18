d806b6a1156df75e61920957c029afccd785d74e
Hugo Alexander <165641301+Galexhu@users.noreply.github.com>
2025-10-18 06:10:58 +0000

    jumbo con imagen

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,23 +3,25 @@ import NavBar from "./NavBar";
 import Card from "./Card";
 import Jumbotron from "./Jumbotron";
 
-const Home= () => {
-const imgUrl1 = "https://media.istockphoto.com/id/2065673896/es/foto/rolling-dice-macro.jpg?s=612x612&w=0&k=20&c=gw61Xz6R9WUx65dUqmSErimq5ezPeHeEB-3AkYqpCEk=";
-const imgUrl2 = "https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2670"
-const imgUrl3 = "https://images.unsplash.com/photo-1616449973117-0e1d99c56ed3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074"
-const imgUrl4 = "https://images.unsplash.com/photo-1616141893496-fbc65370493e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070"
+const Home = () => {
+	const imgUrl0 = "https://plus.unsplash.com/premium_photo-1684517010043-7bde3568311e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1471"
+	const imgUrl1 = "https://images.unsplash.com/photo-1551524358-f34c0214781d?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHN1cmZ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=400";
+	const imgUrl2 = "https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2670"
+	const imgUrl3 = "https://images.unsplash.com/photo-1616449973117-0e1d99c56ed3?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074"
+	const imgUrl4 = "https://images.unsplash.com/photo-1616141893496-fbc65370493e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070"
 	return (
-	<>
-		<NavBar />
-		<div className="container ">
-		<Jumbotron />
-		<div className="justify-content-center d-flex mt-5">
-		<Card imageUrl={imgUrl1} />
-		<Card imageUrl={imgUrl2} />
-		<Card imageUrl={imgUrl3} />
-		<Card imageUrl={imgUrl4} />
-		</div>
-		</div>
-	</>);
+		<>
+			<NavBar />
+				<Jumbotron imageUrl={imgUrl0}/>
+			
+			<div className="container">
+				<div className="justify-content-center d-flex mt-5">
+					<Card imageUrl={imgUrl1} />
+					<Card imageUrl={imgUrl2} />
+					<Card imageUrl={imgUrl3} />
+					<Card imageUrl={imgUrl4} />
+					</div>
+			</div>
+		</>);
 };
 export default Home;
--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -1,6 +1,17 @@
-const Jumbotron = () => {
-return (
-    <></>
-)
+
+const Jumbotron = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <div className="card text">
+                <img src={props.imageUrl}  className="card-img-jumbo" style={{width:"rem 50"}} alt="..." /> //no me aplica el cambio de tamaño
+                <div className="card-img-overlay">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small>Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Jumbotron 
